5d433d5f15a8cd08281e3ac2bcbff876c3c767b7
Alex <gherma@uw.edu>
2021-05-24 21:04:19 -0700

    add flipping card functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 export function App(props) {
   let plantArray = props.plantData;
@@ -7,66 +8,87 @@ export function App(props) {
   return(
     <div>
       <h1>Plant Care Site: React</h1>
-      <PlantGrid plantArray={plantArray} />
+      <PlantGrid plantArray={plantArray}/>
     </div>
   );
 }
 
-function PlantText (props) {
-  let plantObject = props.plant;
-  let header = props.header;
-
-  let textContent = "";
-  let textClasses = "";
-
-  if (header === 'Detailed Info') {
-    textContent = plantObject[header];
-    textClasses = "d-none m-0 p-1";
-  } else if (header === 'Care Tip') {
-    textContent = "Care Tip: " + plantObject[header];
-    textClasses = "d-none m-0 p-1";
-  } else {
-    textContent = header + ': ' + plantObject[header];
-    textClasses = "d-none m-1 p-0.5";
-  }
-
-  if (header === 'Light Level' || header === 'Water Level'){
-    textClasses = textClasses + " d-md-block";
-  } else if (header === 'Overall Difficulty') {
-    textClasses = textClasses.replace("d-none", " ");
-    textClasses = textClasses + " base-content";
-  }
-
-  return (
-    <p className={textClasses}>{textContent}</p>
-  );
-}
 
 function PlantCard (props) {
   let plantObject = props.plant;
-  let cardTextHeaders = ['Light Level', 'Water Level', 'Temperature', 'Pot Size', 'Overall Difficulty', 'Toxicity', 'Detailed Info', 'Care Tip'];
-  // props = one plant object
-  let plantText = [];
-  for (let header in plantObject) {
-    if (cardTextHeaders.includes(header)) {
-      let textElement = <PlantText key={header + " " + plantObject['Plant Name']} header={header} plant={plantObject} />
-      plantText.push(textElement);
-    }
+
+  const [isFlipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!isFlipped);
   }
 
   return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
     <div className="card mb-4">
-      <div className="card-body p-2">
-        <img className="img-fluid mb-3 rounded" src={"img/" + plantObject['Image Source']} alt={plantObject['Image Alt']} role="button" aria-label="get plant details"/>
-        <h2 className="card-title">{plantObject['Plant Name']}</h2>
-        <h3 className="card-subtitle font-weight-light text-muted">{plantObject['Latin Name']}</h3>
-        <div className="text-content card-text pt-2">
-          {plantText}
-          <button className="d-none btn btn-light" type="button">Back</button>
-        </div>
-
-      </div>
+      <CardFront plant={plantObject} handleClick={handleClick}/>
     </div>
+
+    <div className="card mb-4">
+      <CardBack plant={plantObject} handleClick={handleClick}/>
+    </div>
+    </ReactCardFlip>
+  );
+}
+
+
+function CardFront (props) {
+  let plantObject = props.plant;
+  let handleClick = props.handleClick;
+
+  return (
+    <div className="card-body p-2">
+      <img onClick={handleClick} className="img-fluid mb-3 rounded" src={"img/" + plantObject['Image Source']} alt={plantObject['Image Alt']} role="button" aria-label="get plant details"/>
+      <h2 className="card-title">{plantObject['Plant Name']}</h2>
+      <h3 className="card-subtitle font-weight-light text-muted">{plantObject['Latin Name']}</h3>
+      <div className="text-content card-text pt-2">
+        <p className="d-none d-md-block m-1 p-0.5">{"Light Level: " + plantObject['Light Level']}</p>
+        <p className="d-none d-md-block m-1 p-0.5">{"Water Level: " + plantObject['Water Level']}</p>
+        <p className="m-1 p-0.5">{"Overall Difficulty: " + plantObject['Overall Difficulty']}</p>
+      </div>
+
+    </div>
+  );
+}
+
+function CardBack (props) {
+  let plantObject = props.plant;
+  let handleClick = props.handleClick;
+
+  return (
+    <div className="card-body p-2">
+      <h2 className="card-title">{plantObject['Plant Name']}</h2>
+      <h3 className="card-subtitle font-weight-light text-muted">{plantObject['Latin Name']}</h3>
+      <div className="text-content card-text pt-2">
+        <p className="m-1 p-0.5">{"Light Level: " + plantObject['Light Level']}</p>
+        <p className="m-1 p-0.5">{"Water Level: " + plantObject['Water Level']}</p>
+        <p className="m-1 p-0.5">{"Temperature: " + plantObject['Temperature']}</p>
+        <p className="m-1 p-0.5">{"Pot Size: " + plantObject['Pot Size']}</p>
+        <p className="m-1 p-0.5">{"Overall Difficulty: " + plantObject['Overall Difficulty']}</p>
+        <p className="m-1 p-0.5">{"Toxicity: " + plantObject['Toxicity']}</p>
+        <p className="m-0 p-1">{plantObject['Detailed Info']}</p>
+        <p className="m-0 p-1"><em>Care Tip: </em>{plantObject['Care Tip']}</p>
+        <button onClick={handleClick} className="btn btn-light" type="button">Back</button>
+      </div>
+
+    </div>
+  );
+}
+
+function FavoriteButton () {
+  const [isFavorited, setFavorite] = useState(false);
+
+  const handleClick = () => {
+    setFavorite(!isFavorited);
+  }
+
+  return (
+
   );
 }
 
@@ -78,7 +100,7 @@ function PlantGrid (props) {
 
   return (
     <div id="plant-cards" className="plantGrid card-columns col-lg-8 col-xl-9">
-      {plantElements}
+    {plantElements}
     </div>
   );
 }
