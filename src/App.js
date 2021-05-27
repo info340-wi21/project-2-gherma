80676556c73d10a899dc9a5c0a99e3522646b1e9
Alex <gherma@uw.edu>
2021-05-26 19:31:02 -0700

    make cards inline

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
+//import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHeart as fasFaHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farFaHeart } from '@fortawesome/free-regular-svg-icons';
 
-library.add(fasFaHeart, farFaHeart);
+//library.add(fasFaHeart, farFaHeart);
 
 export function App(props) {
   let plantArray = props.plantData;
@@ -47,16 +47,13 @@ function CardFront (props) {
   let handleClick = props.handleClick;
 
   return (
-    <div className="card-body p-2">
-      <img onClick={handleClick} className="img-fluid mb-3 rounded" src={"img/" + plantObject['Image Source']} alt={plantObject['Image Alt']} role="button" aria-label="get plant details"/>
-      <h2 className="card-title">{plantObject['Plant Name']}</h2>
-      <h3 className="card-subtitle font-weight-light text-muted">{plantObject['Latin Name']}</h3>
-      <div className="text-content card-text pt-2">
-        <p className="d-none d-md-block m-1 p-0.5">{"Light Level: " + plantObject['Light Level']}</p>
-        <p className="d-none d-md-block m-1 p-0.5">{"Water Level: " + plantObject['Water Level']}</p>
-        <div className="d-flex justify-content-between">
-          <p className="m-1 p-0.5">{"Overall Difficulty: " + plantObject['Overall Difficulty']}</p>
-          <div className="m-1 p-0.5"><FavoriteButton /></div>
+    <div className="card">
+      <div className="card-body p-2">
+        <img onClick={handleClick} className="img-fluid mb-3 rounded" src={"img/" + plantObject['Image Source']} alt={plantObject['Image Alt']} role="button" aria-label="get plant details"/>
+        <h2 className="card-title">{plantObject['Plant Name']}</h2>
+        <h3 className="card-subtitle font-weight-light text-muted">{plantObject['Latin Name']}</h3>
+        <div className="text-content card-text pt-2">
+          <CardText cardSide="front" plant={plantObject} handleClick={handleClick}/>
         </div>
       </div>
     </div>
@@ -68,21 +65,53 @@ function CardBack (props) {
   let handleClick = props.handleClick;
 
   return (
-    <div className="card-body p-2">
-      <h2 className="card-title">{plantObject['Plant Name']}</h2>
-      <h3 className="card-subtitle font-weight-light text-muted">{plantObject['Latin Name']}</h3>
-      <div className="text-content card-text pt-2">
-        <p className="m-1 p-0.5">{"Light Level: " + plantObject['Light Level']}</p>
-        <p className="m-1 p-0.5">{"Water Level: " + plantObject['Water Level']}</p>
-        <p className="m-1 p-0.5">{"Temperature: " + plantObject['Temperature']}</p>
-        <p className="m-1 p-0.5">{"Pot Size: " + plantObject['Pot Size']}</p>
-        <p className="m-1 p-0.5">{"Overall Difficulty: " + plantObject['Overall Difficulty']}</p>
-        <p className="m-1 p-0.5">{"Toxicity: " + plantObject['Toxicity']}</p>
-        <p className="m-0 p-1">{plantObject['Detailed Info']}</p>
-        <p className="m-0 p-1"><em>Care Tip: </em>{plantObject['Care Tip']}</p>
-        <button onClick={handleClick} className="btn btn-light" type="button">Back</button>
+    <div className="card">
+      <div className="card-body p-2">
+        <h2 className="card-title">{plantObject['Plant Name']}</h2>
+        <h3 className="card-subtitle font-weight-light text-muted">{plantObject['Latin Name']}</h3>
+        <div className="text-content card-text pt-2">
+          <CardText cardSide="back" plant={plantObject} handleClick={handleClick}/>
+        </div>
       </div>
+    </div>
+  );
+}
 
+function CardText (props) {
+  const side = props.cardSide;
+  let plantObject = props.plant;
+  let handleClick = props.handleClick;
+
+  let textGroup = (
+  <div>
+    <p className="m-1 p-0.5">{"Light Level: " + plantObject['Light Level']}</p>
+    <p className="m-1 p-0.5">{"Water Level: " + plantObject['Water Level']}</p>
+    <p className="m-1 p-0.5">{"Temperature: " + plantObject['Temperature']}</p>
+    <p className="m-1 p-0.5">{"Pot Size: " + plantObject['Pot Size']}</p>
+    <p className="m-1 p-0.5">{"Overall Difficulty: " + plantObject['Overall Difficulty']}</p>
+    <p className="m-1 p-0.5">{"Toxicity: " + plantObject['Toxicity']}</p>
+    <p className="m-0 p-1">{plantObject['Detailed Info']}</p>
+    <p className="m-0 p-1"><em>Care Tip: </em>{plantObject['Care Tip']}</p>
+    <button onClick={handleClick} className="btn btn-light" type="button">Back</button>
+  </div>
+  )
+
+  if (side === "front") {
+    textGroup = (
+    <div>
+      <p className="d-none d-md-block m-1 p-0.5">{"Light Level: " + plantObject['Light Level']}</p>
+      <p className="d-none d-md-block m-1 p-0.5">{"Water Level: " + plantObject['Water Level']}</p>
+      <div className="d-flex justify-content-between">
+        <p className="m-1 p-0.5">{"Overall Difficulty: " + plantObject['Overall Difficulty']}</p>
+        <div className="m-1 p-0.5"><FavoriteButton /></div>
+      </div>
+    </div>
+    )
+  }
+
+  return (
+    <div>
+      {textGroup}
     </div>
   );
 }
